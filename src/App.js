511775b07086902cb42f5df1edd595d9309a90d5
Your Name <avinashvinod0311@gmail.com>
2023-10-27 01:16:15 +0530

    Fifth commit

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const Wrapper = styled.div`
     z-index: 2;
 
     @media screen and (max-width: 1640px) {
-      transform: scale(1.2) translateX(-20px) translateY(-30px);
+      transform: scale(0.9) translateX(400px) translateY(30px);
     }
 
     @media screen and (max-width: 1440px) {
