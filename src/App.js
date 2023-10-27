7c7774f75279a282e72413c44af6680ab2686e74
Your Name <avinashvinod0311@gmail.com>
2023-10-27 11:50:31 +0530

    Sixth change

--- src/App.js
+++ src/App.js
@@ -37,6 +37,9 @@ const Wrapper = styled.div`
   color: white;
   margin: 0 auto;
   position: relative;
+  height: 100%;
+  width: 100%;
+  overflow-x: hidden;
 
 
   .spline {
@@ -46,41 +49,63 @@ const Wrapper = styled.div`
     right: -10rem;
     z-index: 2;
 
-    @media screen and (max-width: 1640px) {
+    @media screen and (max-width: 1840px) {
       transform: scale(0.9) translateX(400px) translateY(30px);
     }
 
+    @media screen and (max-width: 1590px) {
+      transform: scale(1) translateX(250px) translateY(30px);
+    }
+
     @media screen and (max-width: 1440px) {
-      transform: scale(1) translateX(280px) translateY(20px);
+      transform: scale(1) translateX(200px) translateY(20px);
+    }
+
+    @media screen and (max-width: 1240px) {
+      transform: scale(1) translateX(80px) translateY(20px);
+      z-index: 0;
     }
 
 
     @media screen and (max-width: 1024px) {
-      transform: scale(0.8) translateX(180px) translateY(80px);
+      transform: scale(0.7) translateX(60px) translateY(80px);
+    }
+
+    @media screen and (max-width: 1000px) {
+      transform: scale(0.6) translateX(-10px) translateY(6rem);
+    }
+
+    @media screen and (max-width: 900px) {
+      transform: scale(0.8) translateX(-200px) translateY(6rem);
+      z-index: 0;    
     }
 
     @media screen and (max-width: 800px) {
-      transform: scale(1) translateX(180px);
+      transform: scale(0.8) translateX(-400px) translateY(6rem);
+      z-index: 0;    
     }
 
     @media screen and (max-width: 700px) {
-      transform: scale(1) translateX(170px);
+      transform: scale(0.7) translateX(-400px) translateY(6rem);
+      z-index: 0;
     }
 
     @media screen and (max-width: 600px) {
-      transform: scale(1) translateX(600px);
+      transform: scale(0.7) translateX(50px) translateY(8rem);
       left: 50%;
       margin-left: -500px;
+      z-index: 0;
     }
+
 
     @media screen and (max-width: 500px) {
-      transform: scale(0.9) translateX(760px) translateY(50px);
-      left: 50%;
-      margin-left: -600px;
+      transform: scale(0.5) translateX(200px) translateY(-8rem);
+      z-index: 0;
     }
 
-    @media screen and (max-width: 375px) {
-      transform: scale(0.8) translateX(760px) translateY(85px);
+    @media screen and (max-width: 400px) {
+      transform: scale(0.5) translateX(400px) translateY(-8rem);
+      z-index: 0;
     }
   }
 
@@ -98,12 +123,18 @@ const Content = styled.div `
     gap: 60px;
   }
 
+  @media screen and (max-width: 500px) {
+    gap: 30px;
+  }
+
+
   h1 {
     font-family: 'Spline Sans Mono', monospace;
     font-size: 70px;
     font-weight: bold;
     margin: 0;
     max-width: 500px;
+    position: relative;
 
     @media screen and (max-width: 1024px) {
       font-size: 60px;
@@ -122,7 +153,16 @@ const Content = styled.div `
     }
 
     @media screen and (max-width: 600px) {
-      padding-top: 70px;
+      font-size: 30px;
+      max-width: 290px;
+      top: -2rem;
+    }
+
+    @media screen and (max-width: 500px) {
+      font-size: 13px;
+      max-width: 100px;
+      top: -2rem;
+      left: -4.5rem;
     }
 
   }
@@ -131,6 +171,20 @@ const Content = styled.div `
     font-weight: normal;
     line-height: 150%;
     max-width: 380px;
+    position: relative;
+
+    @media screen and (max-width: 600px) {
+      font-size: 10px;
+      max-width: 190px;
+      top: -2rem;
+    }
+
+    @media screen and (max-width: 500px) {
+      font-size: 10px;
+      max-width: 150px;
+      top: -2rem;
+      left: -4.5rem;
+    }
   }
 
   h1, p, button {
@@ -153,6 +207,16 @@ const Content = styled.div `
     justify-content: center;
     align-items: center;
 
+    position: relative;
+
+    @media screen and (max-width: 500px) {
+      width: 200px;
+      font-size: 10px;
+      padding: 6px 30px;
+
+      left: -4.5rem;
+    }
+
     &:hover {
       border: 1px solid rgba(255, 255, 255, 0.8);
       transform: translateY(-3px);
@@ -170,12 +234,33 @@ const Menu = styled.ul`
   margin: 0 30px 0 100px;
   padding: 0;
 
+  @media screen and (max-width: 500px) {
+    margin: -4.3rem 30px 0 20px;
+  }
+
+
   li {
     list-style: none;
     margin: 0;
 
     button {
       width: 200px;
+
+      @media screen and (max-width: 600px) {
+        margin-left: 40px;
+        width: 150px;
+      }
+
+      @media screen and (max-width: 500px) {
+        margin-left: 80px;
+        width: 150px;
+      }
+
+      @media screen and (max-width: 400px) {
+        margin-left: -2px;
+        width: 150px;
+      }
+
     }
 
     a {
@@ -185,6 +270,11 @@ const Menu = styled.ul`
       padding: 8px 20px;
       text-decoration: none;
       transition: 1s;
+
+      @media screen and (max-width: 950px) {
+        display: none;
+      }
+
 
       &:hover {
         border: 1px solid rgba(255, 255, 255, 0.2);
@@ -204,6 +294,10 @@ const Social = styled.div`
   gap: 30px;
   align-items: center;
 
+  @media screen and (max-width: 500px) {
+    display:none;
+      }
+
   div {
     width: 1px;
     height: 500px;
@@ -213,6 +307,7 @@ const Social = styled.div`
       #6C56EF 33.57%, 
       #1306DD 65.86%, 
       #AA0EB2 100%);
+
   }
 
 `;
